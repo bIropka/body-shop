e47d9db874ab7f16fed250c4d24626b983438f45
bIropka <biropka@gmail.com>
2016-11-11 14:49:14 +0200

    v 0.3

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,5 +1,32 @@
 $(document).ready(function () {
 
+    function getInternetExplorerVersion() {
+        var rv = -1;
+        if (navigator.appName == 'Microsoft Internet Explorer')
+        {
+            var ua = navigator.userAgent;
+            var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+            if (re.exec(ua) != null)
+                rv = parseFloat( RegExp.$1 );
+        }
+        else if (navigator.appName == 'Netscape')
+        {
+            var ua = navigator.userAgent;
+            var re  = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
+            if (re.exec(ua) != null)
+                rv = parseFloat( RegExp.$1 );
+        }
+        return rv;
+    }
+
+    if(getInternetExplorerVersion()!==-1){
+        $('.car').css('display', 'none');
+        $('.car-ie').css('display', 'block');
+    } else {
+        $('.car-ie').css('display', 'none');
+        $('.car').css('display', 'block');
+    }
+
     setTimeout(function() {
         $('.slider').animate({opacity: 1}, 500);
     }, 250);
